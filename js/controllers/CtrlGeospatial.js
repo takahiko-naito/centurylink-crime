a0c8f5271020d777fcde43e90171d182adc1fa7e
Zach Cowell <zach.cowell@gmail.com>
2014-09-27 17:36:04 -0400

    Filter styling

--- js/controllers/CtrlGeospatial.js
+++ js/controllers/CtrlGeospatial.js
@@ -4,6 +4,17 @@ angular.module('cLink.controllers')
 .controller('CtrlGeospatial', 
   function($scope,SvcDataAccess,SvcGeospatial,SvcMetricsVisualization) {
     $scope.chartConfig = {};
+    $scope.button = { 
+      distance: 1
+      , refresh : {
+        click : function() { 
+          console.log('refresh') ;
+        }
+      }
+    };
+    $scope.fromDate;
+    $scope.untilDate = new Date(); 
+
     var callbacks = {
       success: function(data) {
         $scope.mapDefaults.markers = SvcGeospatial.getIncidentLocationMarkers(data);
